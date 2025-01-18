7a4773920ad599e4dcbb93673a13770f6da8b0c4
pranavp-work <pranavp.work@gmail.com>
2025-01-18 12:49:51 +0530

    mern day 10 --50%

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStackOverflow } from '@fortawesome/free-brands-svg-icons';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { requestApi } from '../service/allApi';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Auth({ register }) {
+
+  const navigate = useNavigate()
 
   const [userDetails, setUserDetails] = useState({
     username: "",
@@ -18,7 +22,7 @@ function Auth({ register }) {
   return (
     <>
       <div className="container p-5">
-        <h3 className='text-warning' style={{ marginLeft: '10%' }}><FontAwesomeIcon icon={faArrowLeft} /> Back Home</h3>
+        <h3 className='text-warning' onClick={()=> navigate('/')} style={{ marginLeft: '10%', cursor: 'pointer' }}><FontAwesomeIcon icon={faArrowLeft} /> Back Home</h3>
 
         {/* login */}
         <div className="row">
@@ -45,16 +49,25 @@ function Auth({ register }) {
                   <div>
                     <h5 className='text-center text-white'><FontAwesomeIcon icon={faStackOverflow} /> Project Fair</h5>
                     <h5 className='text-center text-white'>Sign up your account</h5>
-                    <input type="text" placeholder='username' className='w-100 p-1 mt-3 rounded-2 border-0' onChange={(e)=> setUserDetails({...userDetails, username: e.target.value})} />
-                    <input type="text" placeholder='email' className='w-100 p-1 mt-3 rounded-2 border-0' onChange={(e)=> setUserDetails({...userDetails, email: e.target.value})} />
-                    <input type="password" placeholder='password' className='w-100 p-1 mt-3 rounded-2 border-0' onChange={(e)=> setUserDetails({...userDetails, password: e.target.value})} />
+                    <input type="text" placeholder='username' className='w-100 p-1 mt-3 rounded-2 border-0' value={userDetails.username} onChange={(e)=> setUserDetails({...userDetails, username: e.target.value})} />
+                    <input type="text" placeholder='email' className='w-100 p-1 mt-3 rounded-2 border-0' value={userDetails.email} onChange={(e)=> setUserDetails({...userDetails, email: e.target.value})} />
+                    <input type="password" placeholder='password' value={userDetails.password} className='w-100 p-1 mt-3 rounded-2 border-0' onChange={(e)=> setUserDetails({...userDetails, password: e.target.value})} />
                     <button className='btn w-100 p-1 btn-warning mt-3 rounded-2' onClick={async () => {
                       const {username, email, password} = userDetails
                       if(!username || !email || !password) {
-                        alert('Fill the Form Completely to Register!')
+                        toast.warning('Fill the Form Completely to Register!')
                       } else {
                         const result = await requestApi(userDetails)
                         console.log(result);
+                        if(result.status == 200) {
+                          toast.success('Registration Success!');
+                          setUserDetails({ username: "", email: "", password: ""})
+                          navigate('/login')
+                        } else if(result.status == 406) {
+                          toast.warning(result.response.data)
+                        } else {
+                          toast.danger('Something went wrong!')
+                        }
                       }
                     }}>Register</button>
                     <p className='text-white'>Already have an account? <Link to='/login' className='text-danger' style={{ textDecorationColor: 'red' }}>Login</Link></p>
@@ -70,6 +83,7 @@ function Auth({ register }) {
           <div className="col-md-1"></div>
         </div>
       </div>
+      <ToastContainer position='top-center' autoClose={2000} theme='colored' />
     </>
   )
 }
